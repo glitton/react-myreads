6cfe893d83aa88f25d6cc796fb9c2e9a2de62970
Generosa Litton <glcodeworks@gmail.com>
2018-03-25 20:31:42 -0700

    added error handling

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import * as BooksAPI from './BooksAPI';
-import './App.css';
-import Book from './Book';
+import React from "react";
+import { Link } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+import Book from "./Book";
 
 class SearchBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       query: "",
-      searchResults: [],
+      searchResults: []
     };
     this.handleSearch = this.handleSearch.bind(this);
-  };
+  }
 
   // Create method to search for a book and allow user to move it to their collection
   // handleSearch(event) {
@@ -35,23 +35,28 @@ class SearchBooks extends React.Component {
   // Create method to search for a book and allow user to move it to their collection
   handleSearch(event) {
     if (this.state.query !== " ") {
-      this.setState({ query:event.target.value })
-      BooksAPI.search(event.target.value).then(searchResults => {
-        this.props.books.forEach(book => {
-          let shelvedBook = searchResults.find(result => result.id === book.id);
-          if (shelvedBook) {
-            shelvedBook.shelf = book.shelf
+      this.setState({ query: event.target.value });
+
+      BooksAPI.search(event.target.value)
+        .then(searchResults => {
+          if (searchResults.error) {
+            this.setState({ searchResults: [] });
+          } else {
+            this.props.books.forEach(book => {
+              let shelvedBook = searchResults.find(
+                result => result.id === book.id
+              );
+              if (shelvedBook) {
+                shelvedBook.shelf = book.shelf;
+              }
+              this.setState({ searchResults });
+            });
           }
-          this.setState({
-            searchResults: !searchResults || searchResults.error ? [] : searchResults,
-          })
         })
-      })
-    } else {
-      this.setState({
-        searchResults: [],
-        query: ""
-      })
+        .catch(e => {
+          console.log("error " + e);
+          this.setState({ searchResults: [] });
+        });
     }
   }
 
@@ -76,22 +81,17 @@ class SearchBooks extends React.Component {
               placeholder="Search by title or author"
               onChange={this.handleSearch}
             />
-
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
             {this.state.searchResults.map(book => (
-              <Book
-                book={book}
-                key={book.id}
-                moveBook={this.props.moveBook}
-              />
+              <Book book={book} key={book.id} moveBook={this.props.moveBook} />
             ))}
           </ol>
         </div>
       </div>
-    )
+    );
   }
 }
 
